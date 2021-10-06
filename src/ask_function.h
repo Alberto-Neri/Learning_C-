24fd312d98f24f414a949ad61a10a9a22b2cfed1
Alberto Neri <alberto.neri01@universitadipavia.it>
2021-10-06 17:33:50 +0200

    test commit

--- src/ask_function.h
+++ src/ask_function.h
@@ -1,6 +1,6 @@
 #ifndef FUN_H
 #define FUN_H
-#include "struct.h" //only here, in main I'll include this file
+#include "struct.h" //only here, in main I'll include ask_function.h
 
 int askNumber();
 favorite_things ask_favorite_things();
