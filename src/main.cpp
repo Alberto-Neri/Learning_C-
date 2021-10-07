c07fb69091c07ea3457356fee7ce4d75b5a97e08
Alberto Neri <alberto.neri01@universitadipavia.it>
2021-10-07 19:40:53 +0200

    Testing poiter arithmetic
    Learned that arr[2] = *(arr+2) = 2[arr]

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,16 @@
 
 int main()
 {
-	int arr[] {1,2,3};
+	int arr[] {1,2};
 	std::cout << arr << '\n';
 	int* arr_ptr{arr};
 	std::cout << arr_ptr << *arr_ptr << '\n';
+	std::cout << arr[0] << arr[1] << '\n';
 	*arr_ptr = 5;
-	std::cout << arr[0] << '\n';
+	*(arr + 1) = 6; //== arr[1] == 1[arr]
+	std::cout << arr[0] << arr[1] << '\n';
+
+
 	std::cout << "digita 1 per somma, 2 per domande" << '\n';
 	int input = askNumber();
 	switch (input){
